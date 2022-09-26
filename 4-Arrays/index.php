b2a2726c681434ac6f588eea94d2028378af1f0e
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-09-26 15:31:33 -0300

    Arrays

---
+++ 4-Arrays/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/*$ingredientes = [
+    'açucar', 
+    'farinha de trigo', 
+    'ovo', 
+    'leite', 
+    'fermento em pó'
+];
+
+echo $ingredientes[4];
+*/
+
+$numerosDaMegaSena = [5, 8, 23, 29, 34, 51];
+
+echo "Número 1: ".$numerosDaMegaSena[0]."<br/>";
+echo "Número 2: ".$numerosDaMegaSena[1];
