e832b29772a100a6e2b589fd8bb63e79671866b9
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-09-28 20:15:55 -0300

    Cookies no PHP

---
+++ 36-Cookies no PHP/index.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require_once('header.php');
+
+if (isset($_SESSION['aviso'])) {
+    echo $_SESSION['aviso'];
+    $_SESSION['aviso'] = '';
+}
+
+?>
+
+
+
+
+
+<form action="recebedor.php" method="POST">
+    <label for="nome">
+        Nome:
+        <br>
+        <input type="text" name="nome" id="nome">
+    </label>
+    <br><br>
+    <label for="idade">
+        Idade:
+        <br>
+        <input type="text" name="idade" id="idade">
+    </label>
+    <br><br>
+    <label for="email">
+        E-mail:
+        <br>
+        <input type="text" name="email" id="email">
+    </label>
+    <br><br>
+
+    <input type="submit" value="Enviar">
+</form>
