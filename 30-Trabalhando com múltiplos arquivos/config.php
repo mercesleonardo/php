5877e2aa5395746ab4a79f506e3699ae2934ee19
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-09-28 15:53:58 -0300

    Trabalhando com múltiplos arquivos

---
+++ 30-Trabalhando com múltiplos arquivos/config.php
@@ -0,0 +1,4 @@
+<?php
+
+$usuario = 'Leonardo';
+$senha = 123456;
---
+++ 30-Trabalhando com múltiplos arquivos/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//include('abc.php'); // Não impede que o site funcione caso dê algum erro.
+require_once('config.php');
+require_once('header.php');
+require_once('header.php');
+
+echo 'Conteúdo do site...<br/>';
+
+echo 'NOME DO USUARIO: '.$usuario;
