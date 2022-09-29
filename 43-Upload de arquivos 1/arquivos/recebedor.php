d32ebab01c719899c6ff58abfd702fbc02b23ddd
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-09-29 16:20:04 -0300

    Upload de arquivos

--- 43-Upload de arquivos 1/arquivos/recebedor.php
+++ 43-Upload de arquivos 1/arquivos/recebedor.php
@@ -3,5 +3,14 @@
 echo '<pre>';
 print_r($_FILES);
 
-$nome = $_FILES['arquivo'] ['name'];
-move_uploaded_file($_FILES['arquivo'] ['tmp_name'], '../arquivos/'.$nome);
+$permitidos = ['image/jpeg', 'image/png', 'image/jpg'];
+
+if(in_array($_FILES['arquivo']['type'], $permitidos)) {
+    //$nome = $_FILES['arquivo'] ['name'];
+    $nome = md5(time().rand(0, 1000)).'.jpg';
+    move_uploaded_file($_FILES['arquivo'] ['tmp_name'], '../arquivos/'.$nome);
+
+    echo 'Arquivo salvo com sucesso!';
+} else {
+    echo 'Arquivo não permitido';
+}
