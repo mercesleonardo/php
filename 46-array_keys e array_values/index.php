69f7d7b35ccc0e841f6efdead0c538fa5e1372c1
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-01 08:29:03 -0300

    array_keys e array_values

---
+++ 46-array_keys e array_values/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$array = [
+    'nome' => 'Leonardo',
+    'idade' => 36,
+    'empresa' => 'Hoop',
+    'cor' => 'pardo',
+    'profissao' => 'programador'
+];
+
+/*
+$chaves = array_keys($array);
+print_r($chaves);
+*/
+
+$valores = array_values($array);
+print_r($valores);
