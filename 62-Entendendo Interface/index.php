06cf336b16808af5385b3a5e98a3d9907809dea9
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-04 11:37:00 -0300

    Entendendo Interface

---
+++ 62-Entendendo Interface/index.php
@@ -0,0 +1,34 @@
+<?php
+
+interface Database {
+    public function listarProdutos();
+    public function adicionarProduto();
+    public function alterarProduto();
+}
+
+class MysqlDB implements Database {
+    public function listarProdutos() {
+
+    }
+    public function adicionarProduto() {
+        echo "Adicionando com Mysql";
+    }
+    public function alterarProduto() {
+
+    }
+}
+
+class OracleDB implements Database {
+    public function listarProdutos() {
+
+    }
+    public function adicionarProduto() {
+        echo "Adicionando Oracle";
+    }
+    public function alterarProduto() {
+
+    }
+}
+
+$db = new MysqlDB();
+$db->adicionarProduto();
