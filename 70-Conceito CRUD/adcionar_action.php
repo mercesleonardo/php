b57b93c05bf6bcedd5b1363814dd9ecc5a63c372
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-05 16:50:04 -0300

    Read: Lendo dados com PDO

--- 70-Conceito CRUD/adcionar_action.php
+++ 70-Conceito CRUD/adcionar_action.php
@@ -7,13 +7,23 @@ $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
 if($name && $email) {
 
-    $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES(:name, :email)");
-    $sql->bindValue(':name',$name);
-    $sql->bindValue(':email',$email);
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $sql->bindValue(':email', $email);
     $sql->execute();
 
-    header("Location: index.php");
-    exit;
+    if($sql->rowCount() === 0) {
+
+        $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES(:name, :email)");
+        $sql->bindValue(':name',$name);
+        $sql->bindValue(':email',$email);
+        $sql->execute();
+
+        header("Location: index.php");
+        exit;
+    } else {
+        header('Location: adicionar.php');
+        exit;
+    }
 
 } else {
     header('Location: adicionar.php');
