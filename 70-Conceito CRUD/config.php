0560a2a00d4318938c154b2f7684f2a13a262c9e
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-05 15:30:12 -0300

    Conceito CRUD

---
+++ 70-Conceito CRUD/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_name = "teste";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "861907";
+
+$pdo = new PDO("mysql:dbname".$db_name.";host=".$db_host, $db_user, $db_pass );
---
+++ 70-Conceito CRUD/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+CRUD
+
+C -> CREATE
+R-> READ
+U-> UPDATE
+D-> DELETE
+*/ 
+require "config.php";
+?>
+
+<a href="adicionar.php">ADICIONAR USUÁRIO</a>
+
+<table border="1" width="100%">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>EMAIL</th>
+        <th>ACOES</th>
+    </tr>
+</table>
+
+
